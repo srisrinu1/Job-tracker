f36a6ac8ec6566d0e03a22337ccbfc57bc3dde25
srisrinu1 <srinivasreddychalla133@gmail.com>
2023-08-20 23:25:53 -0400

    Added PORT through .env

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express=require('express');
 const app=express();
+require('dotenv').config();
 
+
+const PORT=process.env.PORT || 5001;
 
 app.get("/",(req,res)=>{
   res.status(200).json({message:'Test!'})
 })
 
-app.listen(5000,()=>{
-    console.log('Listening on port 5000');
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`);
 })
