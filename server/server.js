1a0bef6d457a5e55801e3281af3e8361a049251b
srisrinu1 <srinivasreddychalla133@gmail.com>
2023-08-24 01:09:22 -0400

    Setup the basic router

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 const express=require('express');
 const app=express();
+const bodyParser=require('body-parser');
 require('dotenv').config();
 
 
 const PORT=process.env.PORT || 5001;
 
+
+app.use(express.json())
+
 app.get("/",(req,res)=>{
   res.status(200).json({message:'Test!'})
+});
+
+app.post("/",(req,res)=>{
+  res.status(201).json(req.body)
 })
 
 app.listen(PORT,()=>{
