b58412e97748532edd40f9e7a456cf1d5f4b3fc3
srisrinu1 <srinivasreddychalla133@gmail.com>
2023-08-31 00:43:10 -0400

    Added errorHandler middleware

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 const express=require('express');
 const app=express();
 const indexRouter=require('./api/routes/index');
+const errorHandler = require('./api/middlewares/errorHandler');
+
+app.use(express.json())
+
 
 app.use('/',indexRouter);
+app.use(errorHandler)
 
 module.exports=app;
