25f6288948623e835835686ebf2e13764cebfde0
srisrinu1 <srinivasreddychalla133@gmail.com>
2023-08-31 00:16:08 -0400

    Added new routes

--- server/server.js
+++ server/server.js
@@ -2,20 +2,30 @@ const express=require('express');
 const app=express();
 const bodyParser=require('body-parser');
 require('dotenv').config();
+const ConnectToDb=require('./config/db-connection');
+const register=require('./api/routes/auth');
+const errorHandler = require('./api/middlewares/errorHandler');
 
 
 const PORT=process.env.PORT || 5001;
+ConnectToDb();
 
 
 app.use(express.json())
 
-app.get("/",(req,res)=>{
-  res.status(200).json({message:'Test!'})
-});
+// app.get("/",(req,res)=>{
+//   res.status(200).json({message:'Test!'})
+// });
 
-app.post("/",(req,res)=>{
-  res.status(201).json(req.body)
-})
+// app.post("/",(req,res)=>{
+//   res.status(201).json(req.body)
+// })
+
+app.use('/api/user',register);
+app.use(errorHandler);
+
+// console.log(register)
+
 
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`);
