1ea53dd05b2d7aa9fd6f636caf5eafc25e81433a
srisrinu1 <srinivasreddychalla133@gmail.com>
2023-08-31 00:16:22 -0400

    added auth controller

--- server/api/controllers/authController.js
+++ server/api/controllers/authController.js
@@ -1,14 +1,27 @@
 const asyncHandler=require('express-async-handler');
 const authService=require('../../services/authService');
 
+
 const register=asyncHandler(async(req,res,next)=>{
     const {name,email,password,role}=req.body;
-    const user=await authService.registerUser({name,email,password,role});
-    res.status(201).json({
-        success:true,
-        message:'User is registered',
-        data:user
-    })
+    if(!name || !email || !password || !role){
+        res.status(400);
+    }
+    const userAvailable=await authService.findUser({name,email,password,role});
+    console.log("Line 11:",userAvailable);
+    console.log(!userAvailable);
+    if(!userAvailable){
+        const user=await authService.registerUser({name,email,password,role});
+        res.status(201).json({
+            success:true,
+            message:'User is registered',
+            data:user
+        })
+     }
+     res.status(409);
+     throw new Error("User already exists!")
+
+
 });
 
 module.exports={register}
