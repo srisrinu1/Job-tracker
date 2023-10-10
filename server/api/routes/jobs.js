559c39317a94d6aa75878bb9798e82a8baf4d581
srisrinu1 <srinivasreddychalla133@gmail.com>
2023-10-09 22:50:15 -0400

    Added authrntication middleware

--- server/api/routes/jobs.js
+++ server/api/routes/jobs.js
@@ -2,6 +2,8 @@ const express=require('express');
 const router=express.Router();
 const auth= require('../middlewares/auth');
 const { getJob } = require('../controllers/jobsController');
+const validateAuthentication=require('../middlewares/auth');
 
+router.use(auth.validateAuthentication);
 router.get('/:id',getJob);
 module.exports=router;
