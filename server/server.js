aa26883a7f367360d2b0eee31bc80fcce62c9d9a
srisrinu1 <srinivasreddychalla133@gmail.com>
2023-10-11 20:59:42 -0400

    Removed the extra function call

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const errorHandler = require('./api/middlewares/errorHandler');
 //setting .env file variables
 dotenv.config({path:'./.env'})
 const PORT=process.env.PORT || 5001;
-ConnectToDb();
+// ConnectToDb();
 
 
 
