a034352fbd45435cff18c55d77e6e84461eab85e
srinivasa reddy challa <srinivasreddychalla133@gmail.com>
2024-08-15 23:15:03 -0400

    Added the index for file exporting

---
+++ server/services/index.js
@@ -0,0 +1,8 @@
+const authService=require('./services/authService');
+const userService=require('./services/userService');
+const tokenService=require('./services/tokenService');
+module.exports={
+    authService,
+    userService,
+    tokenService
+ };
