22032795c420523c415d8f16c454b4b10a0d9298
srinivasa reddy challa <srinivasreddychalla133@gmail.com>
2024-08-26 17:03:06 -0400

    Fixed the bugs in the tokenService

--- server/services/tokenService.js
+++ server/services/tokenService.js
@@ -2,7 +2,7 @@ const jwt=require('jsonwebtoken');
 const moment = require('moment');
 const Token=require('../models/tokenModel');
 const config=require('../config/config');
-
+const {tokenTypes}=require('../config/token');
 
 
 //Generate JWT token
@@ -12,7 +12,7 @@ const config=require('../config/config');
  //secret: JWT secret
  //default: JWT secret from config.js
  //returns: JWT token string
-const generateJWT=(id,expires,type,secret=jwt.secret)=>{
+const generateJWT=(id,expires,type,secret=config.jwt.secret)=>{
     const payload={
         sub:id,
         iat:moment().unix(),
@@ -29,7 +29,7 @@ const generateJWT=(id,expires,type,secret=jwt.secret)=>{
  //blacklisted: optional, default: false
  //returns: Promise<Token>
  //throws: Error if token already exists in the database
-const saveToken=(token,userId,type,blacklisted=false)=>{
+const saveToken=(token,userId,expires,type,blacklisted=false)=>{
     const newToken=new Token({
         token,
         user:userId,
@@ -43,12 +43,13 @@ const saveToken=(token,userId,type,blacklisted=false)=>{
 
 //Generating access and refresh tokens
 //user: User object
+
 const generateAuthTokens=async(user)=>{
     const accessTokenExpires=moment().add(config.jwt.accessTokenExpires,'minutes');
     const refreshTokenExpires=moment().add(config.jwt.refreshExpirationDays,'days');
     const accessToken=generateJWT(user.id,accessTokenExpires,tokenTypes.ACCESS);
     const refreshToken=generateJWT(user.id,refreshTokenExpires,tokenTypes.REFRESH);
-    await saveToken(refreshToken,user.id,tokenTypes.REFRESH);
+    await saveToken(refreshToken, user.id, refreshTokenExpires, tokenTypes.REFRESH);
     return {
         access:{
             token:accessToken,
@@ -62,8 +63,27 @@ const generateAuthTokens=async(user)=>{
 
 }
 
+//Verifying the token
+ //token: JWT token string
+ //secret: JWT secret
+ //default: JWT secret from config.js
+ //returns: Promise<boolean>
+ //throws: Error if token is invalid or expired
+const verifyToken=(token,type)=>{
+    const payload=jwt.verify(token,config.jwt.secret);
+    const tokenDoc=Token.findOne({token,type,user:payload.sub,blacklisted:false});
+    if(!tokenDoc){
+        throw new Error("Token doesn't exist or is blacklisted");
+    }
+    return tokenDoc;
+
+}
+
+
 module.exports={
     generateJWT,
     saveToken,
-    generateAuthToken
+    generateAuthTokens,
+    verifyToken
+    
 }
