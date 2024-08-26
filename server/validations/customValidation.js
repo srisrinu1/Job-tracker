69599d8139656ad07ea7809583312fffdd3513c5
srinivasa reddy challa <srinivasreddychalla133@gmail.com>
2024-08-25 21:02:00 -0400

    Added the functionality for validating the userId

--- server/validations/customValidation.js
+++ server/validations/customValidation.js
@@ -14,4 +14,12 @@ const passwordValidator=(value,helpers)=>{
 
 }
 
+//Validate the userId mongo 
+const ObjectId=(value,helpers)=>{
+  if (!value.match(/^[0-9a-fA-F]{24}$/)) {
+    return helpers.message('"{{#label}}" must be a valid mongo id');
+  }
+  return value;
+}
+
 module.exports=passwordValidator;
