8ca0602b2200c167d5dc4009bcacb6d0828bc464
srinivasa reddy challa <srinivasreddychalla133@gmail.com>
2024-08-26 17:18:40 -0400

    Updated the app file

--- server/app.js
+++ server/app.js
@@ -1,21 +1,43 @@
 const express=require('express');
 const app=express();
 const cors=require('cors');
-const indexRouter=require('./api/routes/index');
-const dnsServer=require('./utils/dns-server');
+const passport=require('passport');
+// const indexRouter=require('./api/routes/index');
+// const dnsServer=require('./utils/dns-server');
+const routes=require('./api/routes');
+const {jwtStrategy}=require('./config/passport');
+const APIError=require('./utils/APIError');
 const cookieParser=require('cookie-parser');
 const dns = require('dns');
-const errorHandler = require('./api/middlewares/errorHandler');
+// const errorHandler = require('./api/middlewares/errorHandler');
+const errorHandler=require('./api/middlewares/error');
+const timeout=require('connect-timeout');
+app.use(timeout('5s'))
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(cors());
+app.options('*', cors());
+
+app.use(passport.initialize());
+passport.use('jwt', jwtStrategy);
+
+app.use('/v1',routes);
+
+
+app.use((req, res, next) => {
+  next(new APIError(httpStatus.NOT_FOUND, 'Not found'));
+});
 
 
 
 
+app.use(errorHandler);
 
-app.use('/',indexRouter);
-app.use(errorHandler)
+
+
 
 module.exports=app;
+
+
