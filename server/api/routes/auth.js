4ce15365a6bbf0074296e3e94744fba265920cfc
srinivasa reddy challa <srinivasreddychalla133@gmail.com>
2024-08-26 17:15:45 -0400

    updated the route related features

--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -1,13 +1,15 @@
 const express=require('express');
 const router=express.Router();
-const validateUser=require('../middlewares/validators/userValidator');
+const validate=require('../middlewares/validate');
+const {authValidation,customValidation}=require('../../validations');
 const {
     register,
     loginUser
 }=require('../controllers/authController');
+const authController=require('../controllers/authController');
 
 
-router.post('/register',validateUser,register);
+router.post('/register',validate(authValidation.register),authController.register);
 router.post('/login',loginUser)
 
 module.exports=router;
