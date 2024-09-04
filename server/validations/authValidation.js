90a578b8f833b750ae29036e0ea2bdca02b73917
srinivasa reddy challa <srinivasreddychalla133@gmail.com>
2024-09-04 14:20:18 -0400

    Made few changes to register and login validation by adding role field

--- server/validations/authValidation.js
+++ server/validations/authValidation.js
@@ -2,17 +2,36 @@ const joi=require('joi');
 const passwordValidator=require('./customValidation');
 const register={
     body:joi.object().keys({
-        email:joi.string().required().email(),
-        password:joi.string().required().custom(passwordValidator),
-        name:joi.string().required(),
+        email:joi.string().required().email().messages({
+            'string.email': 'Please enter a valid email address',
+            'any.required': 'Email is required',
+        }),
+        password:joi.string().required().custom(passwordValidator).messages({
+            'any.required': 'Password is required',
+        }),
+        name:joi.string().required().messages({
+            'any.required': 'Name is required',
+        }),
+        role:joi.string().valid('user', 'admin').required().messages({
+            'any.only': 'Role must be either user or admin',
+            'any.required': 'Role is required',
+        }),
     })
 }
 
 const login={
-    body:joi.object().keys({
-        email:joi.string().required().email(),
-        password:joi.string().required().custom(passwordValidator)
-    })
+    body: joi.object().keys({
+        email: joi.string().required().email().messages({
+            'string.email': 'Please enter a valid email address',
+            'any.required': 'Email is required',
+        }),
+        password: joi.string().required().custom(passwordValidator).messages({
+            'any.required': 'Password is required',
+        }),
+        role: joi.string().valid('user', 'admin').optional().messages({
+            'any.only': 'Role must be either user or admin',
+        }),
+    }),
 }
 
 const logout={
